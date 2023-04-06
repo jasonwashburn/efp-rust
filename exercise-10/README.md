22e955c30275e4c35f7b6106a4e65ae00dd3199b
Jason Washburn <jason.washburn@gmail.com>
2023-04-06 04:40:29 -0500

    finished exercise 10

--- exercise-10/README.md
+++ exercise-10/README.md
@@ -9,13 +9,12 @@ out the subtotal, tax amount, and total.
 ## Example Output
 
 ```plaintext
-Enter the price of item 1: 25
-Enter the quantity of item 1: 2
-Enter the price of item 2: 10
-Enter the quantity of item 2: 1
-Enter the price of item 3: 4
-Enter the quantity of item 3: 1
-Subtotal: $64.00
-Tax: $3.52
-Total: $67.52
+Enter the price of the item or <return> if no more items: 2.25
+Enter the quantity of the item or <return> if no more items: 3
+Enter the price of the item or <return> if no more items: .75
+Enter the quantity of the item or <return> if no more items: 2
+Enter the price of the item or <return> if no more items:
+Subtotal: $8.25
+Tax: $0.45
+Total: $8.70
 ```
--- exercise-10/src/main.rs
+++ exercise-10/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
 fn prompt_user_for_float(prompt: &str) -> Option<f32> {
     let mut input = String::new();
-    println!("{prompt} ");
+    print!("{prompt} ");
     stdout().flush().expect("Unable to flush stdout!");
     stdin()
         .read_line(&mut input)
@@ -24,7 +24,7 @@ fn prompt_user_for_float(prompt: &str) -> Option<f32> {
 
 fn prompt_user_for_int(prompt: &str) -> Option<i32> {
     let mut input = String::new();
-    println!("{prompt} ");
+    print!("{prompt} ");
     stdout().flush().expect("Unable to flush stdout!");
     stdin()
         .read_line(&mut input)
@@ -36,10 +36,10 @@ fn prompt_user_for_int(prompt: &str) -> Option<i32> {
         Some(input.parse::<i32>().expect("Unable to parse input to i32"))
     }
 }
-fn get_single_item_from_user() -> Option<(i32, f32)> {
+fn get_single_item_from_user() -> Option<(i32, i32)> {
     let price =
         match prompt_user_for_float("Enter the price of the item or <return> if no more items:") {
-            Some(value) => value,
+            Some(value) => (value * 100.0) as i32,
             None => return None,
         };
     let quantity =
@@ -49,8 +49,8 @@ fn get_single_item_from_user() -> Option<(i32, f32)> {
         };
     Some((quantity, price))
 }
-fn get_items_from_user() -> Vec<(i32, f32)> {
-    let mut items: Vec<(i32, f32)> = Vec::new();
+fn get_items_from_user() -> Vec<(i32, i32)> {
+    let mut items: Vec<(i32, i32)> = Vec::new();
     loop {
         let item = get_single_item_from_user();
         match item {
@@ -61,22 +61,22 @@ fn get_items_from_user() -> Vec<(i32, f32)> {
     items
 }
 
-fn calculate_totals(items: Vec<(i32, f32)>) -> (f32, f32, f32) {
-    let mut subtotal: f32 = 0.0;
+fn calculate_totals(items: Vec<(i32, i32)>) -> (i32, i32, i32) {
+    let mut subtotal: i32 = 0;
     for item in items {
-        subtotal += item.0 as f32 * item.1;
+        subtotal += item.0 * item.1;
     }
-    let tax = subtotal * (TAX_PERCENT_RATE / 100.0);
+    let tax = (subtotal as f32 * (TAX_PERCENT_RATE / 100.0)) as i32;
     let total = subtotal + tax;
     (subtotal, tax, total)
 }
 
-fn print_output(totals: (f32, f32, f32)) {
-    let subtotal = totals.0;
-    let tax = totals.1;
-    let total = totals.2;
+fn print_output(totals: (i32, i32, i32)) {
+    let subtotal = totals.0 as f32 / 100.0;
+    let tax = totals.1 as f32 / 100.0;
+    let total = totals.2 as f32 / 100.0;
 
-    println!("Subtotal: {subtotal:.02}");
-    println!("Tax: {tax}");
-    println!("Total: {total}");
+    println!("Subtotal: ${subtotal:.02}");
+    println!("Tax: ${tax:.02}");
+    println!("Total: ${total:.02}");
 }
